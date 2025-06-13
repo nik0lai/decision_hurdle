81bac374a19129b007e139bf343dae7582c6c288
nicolas <nicolas.csanchez@gmail.com>
2025-06-13 11:14:14 +0200

    script to run main analysis

---
+++ run_main_analysis.R
@@ -0,0 +1,9 @@
+
+source('main_analysis/01_filter_data.R')
+source('main_analysis/02_prop_present_reproduction_exp1.R')
+source('main_analysis/03_reproduction_distribution_exp1.R')
+source('main_analysis/04_prop_present_reproduction_exp2.R')
+source('main_analysis/05_reproduction_distribution_exp2.R')
+source('main_analysis/06_reproduction_distribution_all_trials.R')
+source('main_analysis/07_overall_reproduction_exp3.R')
+source('main_analysis/08_reproduction_distribution_exp3.R')
